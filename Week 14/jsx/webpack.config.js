6ad0440fdcd6405adfdbf7e2ab54485812541772
lee1409 <leerun1409@gmail.com>
2021-01-29 22:36:53 +0800

    init packages

---
+++ Week 14/jsx/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [['@babel/plugin-transform-react-jsx', {pragma: 'createElement'}]]
+          }
+        }
+      }
+    ]
+  },
+  mode: 'development',
+  devtool: 'source-map',
+  watch: true
+}
