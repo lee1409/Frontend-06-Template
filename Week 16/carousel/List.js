0a26fd55af57c1fe047a25e84e142866caedfd02
lee1409 <leerun1409@gmail.com>
2021-02-22 00:24:50 +0800

    update week 16

---
+++ Week 16/carousel/List.js
@@ -0,0 +1,21 @@
+import { Component, ATTRIBUTE } from "./framework.js";
+
+export { STATE, ATTRIBUTE } from "./framework.js";
+
+export class List extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    this.children = this[ATTRIBUTE].data.map(this.template);
+    debugger;
+    this.root = (<div>{this.children}</div>).render();
+    return this.root;
+  }
+
+  appendChild(child) {
+    this.template = child;
+    this.render();
+  }
+}
---
+++ Week 16/carousel/webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: "./main.js",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [
+              [
+                "@babel/plugin-transform-react-jsx",
+                { pragma: "createElement" },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  mode: "development",
+  devtool: "inline-source-map",
+  watch: true,
+  devServer: {
+    contentBase: "./dist",
+  },
+};
