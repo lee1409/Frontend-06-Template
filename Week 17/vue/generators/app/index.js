edc3a0288996c71430870ebb65f841ff2b7f7d67
lee1409 <leerun1409@gmail.com>
2021-02-28 22:24:53 +0800

    add week 17

---
+++ Week 17/vue/generators/app/index.js
@@ -0,0 +1,66 @@
+var Generator = require("yeoman-generator");
+
+module.exports = class extends Generator {
+  async prompting() {
+    this.answers = await this.prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "Your title name",
+        default: this.appname, // Default to current folder name
+      },
+    ]);
+  }
+  writing() {
+    this.destinationRoot(this.answers.title);
+
+    const pkg = {
+      name: this.answers.title,
+      version: "1.0.0",
+      description: "",
+      main: "generators/app/index.js",
+      scripts: {
+        start: "yarn webpack",
+        test: 'echo "Error: no test specified" && exit 1',
+      },
+      author: "",
+      license: "ISC",
+      dependencies: {},
+      devDependencies: {},
+    };
+
+    this.fs.extendJSON(this.destinationPath("package.json"), pkg);
+    this.fs.copyTpl(
+      this.templatePath("index.html"),
+      this.destinationPath("src/index.html"),
+      { title: this.answers.title }
+    );
+    this.fs.copyTpl(
+      this.templatePath("index.js"),
+      this.destinationPath("src/index.js")
+    );
+    this.fs.copyTpl(
+      this.templatePath("HelloWorld.vue"),
+      this.destinationPath("src/HelloWorld.vue")
+    );
+    this.fs.copyTpl(
+      this.templatePath("webpack.config.js"),
+      this.destinationPath("webpack.config.js")
+    );
+  }
+  install() {
+    this.npmInstall(["vue"], { "save-dev": false });
+    this.npmInstall(
+      [
+        "webpack",
+        "webpack-cli",
+        "vue-loader",
+        "vue-style-loader",
+        "css-loader",
+        "vue-template-compiler",
+        "copy-webpack-plugin",
+      ],
+      { "save-dev": true }
+    );
+  }
+};
